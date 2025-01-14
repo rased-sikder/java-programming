11382736d28cc99f646e98fb0df1e8c0846bc411
rased-sikder <mdporagbsmrstu@gmail.com>
2025-01-14 23:40:23 +0600

    hello world and different types of prints

---
+++ basic/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+/*
+
+public: Makes the class or method accessible everywhere.
+class: Defines a blueprint for objects.
+void: Specifies the method returns no value.
+main: The entry point for the program where execution begins.
+System.out.println(): Prints text to the console
+static: means the method or variable belongs to the class itself, not to any specific object, allowing it to be accessed without creating an instance of the class.
+
+
+public static void main(String[] args):
+It’s the main method where the program starts, accessible everywhere (public), belongs to the class (static), doesn’t return anything (void), and accepts command-line arguments as a String array (String[] args).
+
+ */
