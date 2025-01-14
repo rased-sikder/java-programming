25e436e15f7aa7f9d97e04fff4679b3f52a71f22
rased-sikder <mdporagbsmrstu@gmail.com>
2025-01-14 23:49:19 +0600

    Initial commit

---
+++ basic/VariousPrints.java
@@ -0,0 +1,19 @@
+public class VariousPrints {
+    public static void main(String[] args){
+
+// Prints text on the same line without adding a newline character
+        System.out.print("Hello");
+        System.out.print(" World");
+
+// Prints text followed by a new line character, moving the cursor to the next line.
+        System.out.println("Hello");
+        System.out.println("World");
+
+// Formats and prints the output with specified formatting options.It's useful for formatting numbers, stings,and other data.
+
+        String Name;
+        Integer Age;
+        System.out.printf("Name: %s, Age: %d\n", "Alice", 25);
+
+    }
+}
